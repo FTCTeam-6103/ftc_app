070839b006c9d4ba2adb6839c1aedab3edfd65d8
gigaxephones <watkins.michael@jdragonmail.us>
2015-10-06 16:28:18 -0500

    Added MainTeleOp OpMode and registered it in OpMode manager

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -56,10 +56,13 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
 
     manager.register("NullOp", NullOp.class);
+    manager.register("MainTeleOp", MainTeleop.class); //Register TeleOp OpMode
 
+   /* //Uneeded opModes
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
+   */
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MainTeleop.java
@@ -0,0 +1,68 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+/**
+ * Created by 6103 on 10/6/2015.
+ * MainTeleOp
+ */
+
+//Imports
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class MainTeleop extends OpMode{
+    /*
+        Members
+     */
+    //Drive Motors
+    DcMotor leftDrive_wheel;
+    DcMotor leftDrive_assembly;
+    DcMotor rightDrive_wheel;
+    DcMotor rightDrive_assembly;
+
+    //---Constants---
+
+    //Constant for assembly motor speed when triggers/bumpers are pressed
+    final double assemblyDriveValue = 0.9;
+
+    //Motor values
+    double leftWheelValue = 0.0;
+    double rightWheelValue = 0.0;
+
+    /*
+        Implementations
+     */
+    @Override
+    public void init()
+    {
+        //Get refernces to wheel drive motors
+        leftDrive_wheel = hardwareMap.dcMotor.get("drive_left_wheel");
+        rightDrive_wheel = hardwareMap.dcMotor.get("drive_right_wheel");
+
+        //get references to the assembly drive motors
+        leftDrive_assembly = hardwareMap.dcMotor.get("drive_left_assembly");
+        rightDrive_assembly = hardwareMap.dcMotor.get("drive_right_assembly");
+    }
+
+    @Override
+    public void loop() {
+        //Motor drive values
+        leftWheelValue = gamepad1.left_stick_y;
+        rightWheelValue = -(gamepad1.right_stick_y);
+
+        //Write wheel values to motor
+        leftDrive_wheel.setPower(leftWheelValue);
+        rightDrive_wheel.setPower(rightWheelValue);
+
+        //Write trigger value to assembly forward
+        leftDrive_assembly.setPower((gamepad1.left_trigger > 0) ? assemblyDriveValue:0);
+        rightDrive_assembly.setPower((gamepad1.right_trigger > 0) ? assemblyDriveValue:0);
+
+
+        //Write bumper value to assembly backward
+        leftDrive_assembly.setPower((gamepad1.left_bumper)? -(assemblyDriveValue):0);
+        rightDrive_assembly.setPower((gamepad1.right_bumper) ? -(assemblyDriveValue):0);
+    }
+
+
+
+}
